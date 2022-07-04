bffd7e84075e0d0f8d5b6bcbfd960677868c5a7b
Dup4 <lyuzhi.pan@gmail.com>
2022-07-04 02:26:37 +0000

    feat: add result

---
+++ include/ormxx/interface/result.h
@@ -0,0 +1,34 @@
+#ifndef ORMXX_INTERNAL_RESULT_H
+#define ORMXX_INTERNAL_RESULT_H
+
+#include "result/result.h"
+#include "result/result_or.h"
+
+namespace ormxx {
+
+enum class ErrorCode {
+    OK = 0,
+    OtherError,
+};
+
+inline auto ErrorCodeToStr(ErrorCode error_code) {
+    static const std::map<ErrorCode, std::string> ErrorCodeToStrMap = {
+            {ErrorCode::OK, "OK"},
+            {ErrorCode::OtherError, "OtherError"},
+    };
+
+    if (ErrorCodeToStrMap.count(error_code) == 0) {
+        return ErrorCodeToStrMap.rbegin()->second;
+    }
+
+    return ErrorCodeToStrMap.at(error_code);
+}
+
+using Result = result::Result<ErrorCode>;
+
+template <typename T>
+using ResultOr = result::ResultOr<Result, T>;
+
+}  // namespace ormxx
+
+#endif  // ORMXX_INTERNAL_RESULT_H
